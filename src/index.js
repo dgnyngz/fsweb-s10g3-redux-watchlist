b8b493909303b76d87a0f17a55bbce4011f76a14
dgnyngz <dgnyngz@gmail.com>
2023-04-26 19:31:46 +0300

    s10g3 tamamlandı

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import logger from "redux-logger";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { reducer } from "./reducer";
+import { BrowserRouter } from "react-router-dom";
 
-import { BrowserRouter } from "react-router-dom"
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { composeWithDevTools } from "@redux-devtools/extension";
+import { Provider } from "react-redux";
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
